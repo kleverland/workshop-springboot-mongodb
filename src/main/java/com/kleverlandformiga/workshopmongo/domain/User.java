2499d814e54515a1a6e6a92190cc93a7606b3b11
Kleverland Formiga <kleverland@gmail.com>
2020-04-23 13:44:19 -0300

    Referenciando os posts do usuário

--- src/main/java/com/kleverlandformiga/workshopmongo/domain/User.java
+++ src/main/java/com/kleverlandformiga/workshopmongo/domain/User.java
@@ -1,28 +1,42 @@
 package com.kleverlandformiga.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
-public class User implements Serializable{
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
+
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
 	public User() {
-		
+
 	}
-	
+
 	public User(String id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	public String getId() {
@@ -72,5 +86,5 @@ public class User implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
 }
